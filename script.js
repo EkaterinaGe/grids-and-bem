c24fb147910dc4d3cdf3af9439ef9a6cf9a5f9c1
Ekaterina Gencheva <kvgencheva@gmail.com>
2023-06-26 06:57:59 +0300

    Added swiper

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('click', function(el) {
+    
     if ( el.target.classList.contains('menu__item') ) { 
         let menuAll = document.querySelectorAll('.menu__item');
         menuAll.forEach( function(elem) {
@@ -16,4 +17,48 @@ document.addEventListener('click', function(el) {
         let menuMobile = document.querySelector('.menu-mobile');
         menuMobile.classList.remove('menu-active');
     }
+
+    if ( el.target.classList.contains('swiper__expand') 
+    || el.target.classList.contains('swiper__expand__arrow') 
+    || el.target.classList.contains('swiper__expand__text') ) {
+        
+        let expand = document.querySelector('.swiper__expand__text');
+
+        if ( expand.innerText == 'Показать все' ) {
+            let showCell = document.querySelectorAll('.swiper__hidden');
+            showCell.forEach( function(elem) {
+                elem.style.display = 'flex';
+            })
+
+            expand.innerText = 'Скрыть';
+
+            let arrow = document.querySelector('.swiper__expand__arrow');
+            arrow.style.transform = 'scale(1, -1)';
+        } else {
+            let showCell = document.querySelectorAll('.swiper__hidden');
+            showCell.forEach( function(elem) {
+                elem.style.display = 'none';
+            })
+
+            expand.innerText = 'Показать все';
+
+            let arrow = document.querySelector('.swiper__expand__arrow');
+            arrow.style.transform = 'none';
+            
+            if ( window.innerWidth >= 1120 ) {
+                showCell[0].style.display = 'flex';
+                showCell[1].style.display = 'flex';
+            }
+        }
+    }
 })
+
+if ( window.innerWidth <= 768 ) {
+    const swiper = new Swiper('.swiper', {
+        mousewheel: true,
+        keyboard: true,
+        pagination: {
+          el: '.swiper-pagination',
+        },
+    });
+}
